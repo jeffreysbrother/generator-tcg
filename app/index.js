a57e0190f49a9040d8a5d6bfb77e605a5c6cd3e4
James Cool <jeffreysbrother@gmail.com>
2017-08-08 21:02:06 -0700

    update regex and clarify comment

--- app/index.js
+++ app/index.js
@@ -79,10 +79,11 @@ module.exports = class extends Generator {
       name: 'newDir',
       message: 'What would you like to call it?',
       validate: value => {
-				// this could be better
-				// would be good to also eliminate single spaces from the beginning and end of the string
-				let multipleSpaceRemoval = value.replace(/\s{2,}/g, ' ');
-				valueToArray = multipleSpaceRemoval.split(' ');
+				// this could be better: currently eliminates all tabs and MULTIPLE spaces
+				// (except for those at the beginning and end of the strting)
+				// would best to eliminate single spaces and tabs from the beginning and end of the string
+				let multipleSpaceAndTabRemoval = value.replace(/\s{2,}|\t/g, ' ');
+				valueToArray = multipleSpaceAndTabRemoval.split(' ');
 				valueToArray.forEach(item => {
 					// ensure user input is two letters, a hyphen, and 2-3 digits
 					if (value.match(restrictUserInputPattern)) {
